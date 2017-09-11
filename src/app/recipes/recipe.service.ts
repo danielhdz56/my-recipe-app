57bb734b7aba2210470fd53ace578da95a5dc7ef
Daniel Hernandez <daniel56hdz@gmail.com>
2017-09-11 15:47:56 -0500

    add RecipeService, ShoppingListService.

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,14 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+    private recipes: Recipe[] = [
+        new Recipe('First Recipe', 'This is simply a test', 'http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg'),
+        new Recipe('Second Recipe', 'This is simply another test', 'http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg')
+    ];
+
+    getRecipes(){
+        // slice returns a new array which is an exact copy from the one in this file.
+        // if we omit slice() we would end up, possibly, changing the original array.
+        return this.recipes.slice(); 
+    }
+}
